c070297345303aa3ca547b983a9f3a6638d09954
orxfun <orx.ugur.arikan@gmail.com>
2026-09-15 11:34:12 +0200

    create sort tests

--- tests/soa2.rs
+++ tests/soa2.rs
@@ -78,3 +78,19 @@ fn soa2_parallel_extend_core_matches_generic_pattern() {
         vec![(4, 'd'), (5, 'e')]
     );
 }
+
+#[test]
+fn soa2_sort() {
+    let mut soa = Soa2::new();
+    soa.extend([(1, 'a'), (0, 'c'), (3, 'b'), (2, 'd')]);
+
+    // soa.sort_by1();
+    let mut expected_by1 = Soa2::new();
+    expected_by1.extend([(0, 'c'), (1, 'a'), (2, 'd'), (3, 'b')]);
+    assert_eq!(soa, expected_by1);
+
+    // soa.sort_by2();
+    let mut expected_by2 = Soa2::new();
+    expected_by2.extend([(1, 'a'), (3, 'b'), (0, 'c'), (2, 'd')]);
+    assert_eq!(soa, expected_by2);
+}
